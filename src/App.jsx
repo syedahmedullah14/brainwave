4baba33c156dd9c5e992ecb03a9a5a9b5b18eee7
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-29 17:40:39 +0530

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl flex font-bold underline">Hello world!</h1>
     </>
   );
 }
