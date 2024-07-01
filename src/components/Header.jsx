50abf3ea34659de97d2454bcea999aa50f57807d
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-01 15:21:54 +0530

    added constant values in constant/index.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { brainwave } from "../assets";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwave} alt="brainwave" width={190} height={40} />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
