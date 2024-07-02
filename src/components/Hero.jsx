402520aab1ff89cd750757328c426c292d6a01c7
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-02 18:26:29 +0530

    building Benefits component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
-      crosses
+      crosses4
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
       id="hero"
